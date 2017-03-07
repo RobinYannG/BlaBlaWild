47e51e3a46714c2e875d871ec923e3afb6b6b8cf
RobinYannG <robin.goudy@gmail.com>
2017-03-07 18:19:55 +0100

    ADAPTERS

--- app/src/main/java/fr/wcs/blablawild/ViewSearchItineraryResultsListActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ViewSearchItineraryResultsListActivity.java
@@ -3,11 +3,20 @@ package fr.wcs.blablawild;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class ViewSearchItineraryResultsListActivity extends AppCompatActivity {
 
     private SearchRequestModel data;
+    private ListView mListViewResults;
+    private ListAdapter mResultsAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +28,23 @@ public class ViewSearchItineraryResultsListActivity extends AppCompatActivity {
 
         Toast.makeText(this,data.getDate(), Toast.LENGTH_SHORT).show();
         this.setTitle(data.getDeparture() + " " + ">>" + " " + data.getDestination());
+
+
+
+        mListViewResults = (ListView) findViewById(R.id.driveList);
+        ArrayList<TripResultModel> results = new ArrayList<>();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy-hh:mm");
+
+        try {
+            results.add(new TripResultModel("Bruce", sdf.parse("21/02/2017-15:30"), 15));
+            results.add(new TripResultModel("Clark", sdf.parse("21/02/2017-16:00"), 20));
+            results.add(new TripResultModel("Bary", sdf.parse("21/02/2017-16:30"), 16));
+            results.add(new TripResultModel("Lex", sdf.parse("21/02/2017-17:00"), 40));
+        } catch (ParseException e) {
+        }
+        mResultsAdapter = new TripResultAdapter(this, results);
+
+        mListViewResults.setAdapter(mResultsAdapter);
     }
 }
